8d7a14a685515235028ff122a1a8957024d2d182
Tzuy284 <20521249@gm.uit.edu.vn>
2023-03-31 14:21:38 +0700

    khai bao bien

---
+++ LAB01/Baitap3/Baitap3/Baitap3.ino
@@ -0,0 +1,17 @@
+int ledPins[] = {4,5, 6, 7, 8, 9}; // mảng chứa các chân kết nối LED
+int buttonPin = 3; // chân kết nối nút bấm
+int diceNumber;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for (int i = 0; i < 6; i++) {
+     pinMode(ledPins[i], OUTPUT);
+  }
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
