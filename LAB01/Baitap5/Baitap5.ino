e685cd15f2a2ecd49cd559dea17f1f88a8307fe4
Tzuy284 <20521249@gm.uit.edu.vn>
2023-03-31 16:09:14 +0700

    button press

--- LAB01/Baitap5/Baitap5.ino
+++ LAB01/Baitap5/Baitap5.ino
@@ -3,6 +3,7 @@ int buttonPin = 8;
 int BUTTONstate = 0;
 
 void setup(){
+  Serial.begin(9600);
   for(int i = 0; i<16; i++)
     pinMode(ledPins[i],OUTPUT);
   pinMode(buttonPin, INPUT);
@@ -10,12 +11,12 @@ void setup(){
 }
 void loop()
 {
-BUTTONstate = digitalRead(buttonPin);
   mygame();
 }
 
 void mygame(){
 while(1){
+  BUTTONstate = digitalRead(buttonPin);
   for(int i = 0; i <16; i++){
      if(i==0)
       digitalWrite(ledPins[15],LOW);
@@ -24,7 +25,14 @@ while(1){
     
     digitalWrite(ledPins[i],HIGH);
     delay(1000);
-   
+    if(BUTTONstate == HIGH)
+    {
+      if(i == 0)
+        Serial.println("dung");
+      else
+        Serial.println("sai");
+
+    }
 
   }
 }
