fe2a4462a82fa62c711fad34c1e8c25164fbdb79
Tzuy284 <20521249@gm.uit.edu.vn>
2023-03-31 15:56:10 +0700

    Led go around

--- LAB01/Baitap5/Baitap5.ino
+++ LAB01/Baitap5/Baitap5.ino
@@ -1,11 +1,31 @@
-int L0 = 0, L1 = 1, L2 = 2, L3 = 3, L4 = 4, L5 = 5, L6 = 6, L7 =7, BTN = 8, L8 = 9, L9 = 10, L10 = 11, L11 = 12, L12 = 13;
 int ledPins[]={0,1,2,3,4,5,6,7,9,10,11,12,A0,A1,A2,A3};
 int buttonPin = 8;
+int BUTTONstate = 0;
+
 void setup(){
   for(int i = 0; i<16; i++)
     pinMode(ledPins[i],OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT);
 
 }
 void loop()
-{}
+{
+BUTTONstate = digitalRead(buttonPin);
+  mygame();
+}
+
+void mygame(){
+while(1){
+  for(int i = 0; i <16; i++){
+     if(i==0)
+      digitalWrite(ledPins[15],LOW);
+    else
+      digitalWrite(ledPins[i-1],LOW);
+    
+    digitalWrite(ledPins[i],HIGH);
+    delay(1000);
+   
+
+  }
+}
+}
