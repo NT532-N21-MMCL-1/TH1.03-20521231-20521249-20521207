46e2e755b0afb4c7faafa5dfc165f72cbd474c2e
Tzuy284 <20521249@gm.uit.edu.vn>
2023-04-02 00:08:57 +0700

    update chop tat sau lan va qua man

--- LAB01/Baitap5/Baitap5.ino
+++ LAB01/Baitap5/Baitap5.ino
@@ -1,9 +1,8 @@
-int ledPins[]={0,1,2,3,4,5,6,7,9,10,11,12,A0,A1,A2,A3};
+int ledPins[]={0,13,2,3,4,5,6,7,9,10,11,12,A0,A1,A2,A3};
 int buttonPin = 8;
 int BUTTONstate = 0;
 
-void setup(){
-  Serial.begin(9600);
+void setup(){  Serial.begin(9600);
   for(int i = 0; i<16; i++)
     pinMode(ledPins[i],OUTPUT);
   pinMode(buttonPin, INPUT);
@@ -11,29 +10,53 @@ void setup(){
 }
 void loop()
 {
-  mygame();
+               denchopsaulan();
+
+   mygame(1000);
 }
 
-void mygame(){
+void mygame(int speed){
 while(1){
-  BUTTONstate = digitalRead(buttonPin);
   for(int i = 0; i <16; i++){
-     if(i==0)
+    
+    if(i==0)
       digitalWrite(ledPins[15],LOW);
     else
       digitalWrite(ledPins[i-1],LOW);
     
     digitalWrite(ledPins[i],HIGH);
-    delay(1000);
+    delay(speed);
+    BUTTONstate = digitalRead(buttonPin);
+
     if(BUTTONstate == HIGH)
     {
-      if(i == 0)
-        Serial.println("dung");
+      
+      if(i == 0){
+             denchopsaulan();       
+        if(speed == 1000)
+      		mygame(500);
+        if(speed == 500)
+      		mygame(300);
+      }
+        
       else
-        Serial.println("sai");
+        mygame(1000);
 
     }
 
   }
 }
+}
+void denchopsaulan(){
+  for(int i=0; i <6; i++){
+    for(int i = 0; i <16; i++){
+    	digitalWrite(ledPins[i],HIGH);
+    }
+    delay(200);
+     for(int i = 0; i <16; i++){
+    	digitalWrite(ledPins[i],LOW);
+    }
+        delay(200);
+
+  }
 }
