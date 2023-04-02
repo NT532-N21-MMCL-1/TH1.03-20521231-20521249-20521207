84edb7c2a040788d251de8ed56187c765fab4f1e
13octt <88072761+13octt@users.noreply.github.com>
2023-04-02 22:55:30 +0700

    In giá trị của biến trở ra Serial

--- LAB01/Baitap1/Baitap1.ino
+++ LAB01/Baitap1/Baitap1.ino
@@ -2,12 +2,15 @@
 int ledPins[]={2,3,4,5,6,7,8,9,10,11,12,13};
 
 void setup(){
+  Serial.begin(9600);
   for(int i = 0 ; i < 12; i++)
-    pinMode(ledPins[i],OUTPUT);
+  pinMode(ledPins[i],OUTPUT);
 }
 
 void loop(){
   int inputval = analogRead(A0);
   int x = map(inputval, 0, 1023, 100, 1300);
+  Serial.println(x);
+
 
 }
