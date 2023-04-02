b3395300920c5e8700f984b88812e7cd534c92d8
DUNGJR <dunghoang879@gmail.com>
2023-04-02 20:50:21 +0700

    BT2

---
+++ LAB01/Baitap2/Baitap2.ino
@@ -0,0 +1,50 @@
+#define led1 2
+#define led2 3
+#define led3 4
+
+int delaytime=0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+pinMode(A0, INPUT);
+
+}
+
+void loop()
+{
+  int value = analogRead(A0);
+  
+  delaytime=((5000-100)*(value/1023.0))+100;
+  digitalWrite(led1, HIGH);
+  delay(delaytime);
+  digitalWrite(led2, HIGH);
+  delay(delaytime);
+  Serial.println(value);
+  digitalWrite(led3, HIGH);
+  delay(delaytime);
+  digitalWrite(led3, LOW);
+    delay(delaytime);
+
+  digitalWrite(led2, LOW);
+  delay(delaytime);
+
+
+  digitalWrite(led1, LOW);
+  delay(delaytime);
+
+  Serial.println(delaytime);
+if(delaytime<250)
+{
+  Serial.println("nhanh");
+}
+  else if(delaytime>=150 && delaytime <3000)
+    Serial.println("trung binh");
+    else{
+  Serial.println("cham");}
+    
+  delay(1000); 
+}
