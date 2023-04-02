2f6946538707a9bfe9523737ed61ddbbe37a3678
13octt <88072761+13octt@users.noreply.github.com>
2023-04-02 23:23:19 +0700

    Gọi hàm bật tắt đèn, diều chỉnh số lượng đèn led thông qua biến trở

--- LAB01/Baitap1/Baitap1.ino
+++ LAB01/Baitap1/Baitap1.ino
@@ -12,7 +12,45 @@ void loop()
   int inputval = analogRead(A0);
   int x = map(inputval, 0, 1023,100, 1300);
   Serial.println(x);
-   
+  if(x==100)//gia tri min la 100 
+    ham_tat_den(12);
+  else 
+    if(x >100){
+    ham_mo_den(1);
+    if(x>200){
+      ham_mo_den(2);
+      if(x>300){
+        ham_mo_den(3);
+        if(x >400){
+          ham_mo_den(4);
+          if(x>500){
+            ham_mo_den(5);
+            if(x>600){
+              ham_mo_den(6);
+              if(x>700){
+                ham_mo_den(7);
+                if(x>800){
+                  ham_mo_den(8);
+                  if(x>900){
+                    ham_mo_den(9);
+                    if(x>1000){ 
+                      ham_mo_den(10);
+                      if(x>1100){
+                        ham_mo_den(11);
+                      }
+                      if(x>1200){ 
+                        ham_mo_den(12);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }   
 }
 void ham_mo_den(int value) {
   for(int i = 0 ; i < value; i++){
