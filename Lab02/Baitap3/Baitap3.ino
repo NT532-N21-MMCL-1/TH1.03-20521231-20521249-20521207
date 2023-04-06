b91d03d8aadbf7b81c974660d5a7a0d46a27a8d4
13octt <88072761+13octt@users.noreply.github.com>
2023-04-06 10:03:14 +0700

    Cập nhật đơn vị

--- Lab02/Baitap3/Baitap3.ino
+++ Lab02/Baitap3/Baitap3.ino
@@ -6,3 +6,22 @@ const int DHTTYPE = DHT11;
  
 DHT dht(DHTPIN, DHTTYPE);
  
+void setup() {
+  Serial.begin(9600);
+  dht.begin();         
+}
+ 
+void loop() {
+  float h = dht.readHumidity();   
+  float t = dht.readTemperature(); 
+ 
+  Serial.print("Nhiet do: ");
+  Serial.print(t);   
+  Serial.println(" °C")          
+  Serial.print("Do am: ");
+  Serial.print(h);  
+  Serial.print(" %")            
+  
+  Serial.println();              
+  delay(1000);                     
+}
