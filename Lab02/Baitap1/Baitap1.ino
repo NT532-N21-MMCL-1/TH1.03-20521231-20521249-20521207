1c5a8318794dfc2326cb523a3c4d06f77a128a13
13octt <88072761+13octt@users.noreply.github.com>
2023-04-06 09:35:40 +0700

    Code đo khoảng cách và tốc độ chớp tắt của đèn

--- Lab02/Baitap1/Baitap1.ino
+++ Lab02/Baitap1/Baitap1.ino
@@ -8,3 +8,56 @@ void setup()
   pinMode(echoPin, INPUT);
   pinMode(ledpin,OUTPUT);
 }
+void loop()
+{
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  if (distance >= 200 || distance <= 0)
+  {
+    Serial.println("Out of range");
+  }
+  else{
+  Serial.print(distance);
+  Serial.println(" cm");
+    if(distance <=15){
+      denchoptat(100);
+    }
+    else
+    {
+      if(distance <=30)
+      {
+        denchoptat(300);
+      }
+      else
+        if(distance <= 60)
+        {
+            denchoptat(600);
+        }
+        else 
+        {
+          if(distance <= 100){
+            denchoptat(1000);
+
+          }
+          else
+          {denchoptat(2000);
+
+          }
+        }
+    }
+  }
+  delay(500);
+}
+void denchoptat(int speed){
+  digitalWrite(ledpin,HIGH);
+  delay(speed);
+  digitalWrite(ledpin,LOW);
+  delay(speed);
+}
