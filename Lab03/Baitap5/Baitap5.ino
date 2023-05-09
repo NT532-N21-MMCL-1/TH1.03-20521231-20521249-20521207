cc296d6189cb31a8c61bf0f9370138d02782c1f4
13octt <88072761+13octt@users.noreply.github.com>
2023-05-09 13:26:54 +0700

    setup func

--- Lab03/Baitap5/Baitap5.ino
+++ Lab03/Baitap5/Baitap5.ino
@@ -25,4 +25,36 @@ PubSubClient mqttClient(wifiClient);
 #define LED D3
 DHT dht(DHTPIN, DHTTYPE);
 
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
+  // Connect to MQTT broker
+  mqttClient.setServer(mqtt_server, 1883);
+  while (!mqttClient.connected()) {
+    Serial.println("Connecting to MQTT broker...");
+    if (mqttClient.connect("WemosD1")) {
+      Serial.println("Connected to MQTT broker");
+    } else {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
+
+  // Initialize DHT11 sensor
+  dht.begin();
+}
+
 
