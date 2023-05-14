2c94741ba59a6616c3dfcb05872e7e350a379313
13octt <88072761+13octt@users.noreply.github.com>
2023-05-14 10:54:29 +0700

    Update post return num led

--- Lab03/Baitap2/Baitap2/Baitap2.ino
+++ Lab03/Baitap2/Baitap2/Baitap2.ino
@@ -4,7 +4,7 @@
 
 const char* ssid = "UiTiOt-E3.1";
 const char* password = "UiTiOtAP";
-const char* serverUrl = "http://127.0.0.1:8000/api";
+const char* serverUrl = "http://127.0.0.1:8000/sensor";
 
 const int lightSensorPin = A0;
 const int distanceSensorPin = D2;
@@ -43,4 +43,39 @@ void loop() {
   String payload;
   serializeJson(doc, payload);
 
+  // Send POST request to server
+  HTTPClient http;
+  http.begin(serverUrl);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(payload);
+  if (httpResponseCode == HTTP_CODE_OK) {
+    Serial.println("POST request successful");
+    String response = http.getString();
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, response);
+    int lightCount = doc["data"]["light_count"];
+    if (lightCount == 1) {
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, LOW);
+      digitalWrite(led3Pin, LOW);
+    } else if (lightCount == 2) {
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, HIGH);
+      digitalWrite(led3Pin, LOW);
+    } else if (lightCount == 3) {
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, HIGH);
+      digitalWrite(led3Pin, HIGH);
+    } else {
+      digitalWrite(led1Pin, LOW);
+      digitalWrite(led2Pin, LOW);
+      digitalWrite(led3Pin, LOW);
+    }
+  } else {
+    Serial.printf("POST request failed with code %d\n", httpResponseCode);
+  }
+  http.end();
+
+  // Wait for 5 seconds
+  delay(5000);
 }
