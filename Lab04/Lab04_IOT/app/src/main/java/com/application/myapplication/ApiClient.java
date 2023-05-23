e9adc5f4f5ffbc33caeb50adc39bcd5d97eff08a
13octt <88072761+13octt@users.noreply.github.com>
2023-05-23 21:14:50 +0700

    clean code

--- Lab04/Lab04_IOT/app/src/main/java/com/application/myapplication/ApiClient.java
+++ Lab04/Lab04_IOT/app/src/main/java/com/application/myapplication/ApiClient.java
@@ -9,7 +9,11 @@ public class ApiClient {
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+<<<<<<< HEAD
                     .baseUrl("http://10.45.3.204:8000")
+=======
+                    .baseUrl("http://127.0.0.1:8000")
+>>>>>>> parent of ca619ed (connect retrofit to fastapi successfully)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
